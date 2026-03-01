63b1e1ecbc854e80e3fc7a60105a378234843401
devsamuelv <realalloutmusic@gmail.com>
2026-03-01 01:27:29 -0500

    Added rust toolchain into the nix flake

--- flake.nix
+++ flake.nix
@@ -30,35 +30,16 @@
         naersk-lib = pkgs.callPackage naersk {};
       in
       {
-        # packages.jxl =
-        #   with pkgs.clangStdenv;
-        #   mkDerivation {
-        #     pname = "jxl-lib";
-        #     version = "1.0.0";
-        #     src = nixpkgs.legacyPackages.x86_64-linux.fetchFromGitHub {
-        #       owner = "libjxl";
-        #       repo = "libjxl";
-        #       rev = "8ce9537c989cfc7adff034556c8a4b9469e874d6";
-        #       sha256 = "sha256-PHkk3Fe1WEoF1lJjKUsH7STcZUjr6y251g7oHAnHUME=";
-        #     };
-
-        #     buildInputs = [ pkgs.cmake pkgs.libhwy pkgs.brotli pkgs.libjpeg pkgs.libwebp skcms.packages.x86_64-linux.skcms ];
-        #     cmakeFlags = [ ];
-        #     nativeBuildInputs = [ ];
-        #     buildPhase = ''
-        #       export CC=clang CXX=clang++ PATH="$PATH:${skcms.packages.x86_64-linux.skcms.out}/libskcms.a"
-        #       mkdir build
-        #       cd build
-        #       cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF ..
-        #       cmake --build . -- -j$(nproc)
-        #     '';
-        #   };
-
         packages.default = naersk-lib.buildPackage {
           src = ./.;
+          DEP_JXL_LIB = "${pkgs.libjxl.out}";
+
           buildInputs = [
             # Generic DevTools
             # clang-tools must be first before clang
+            pkgs.pkg-config
+            pkgs.clang
+            pkgs.gcc
             pkgs.clang-tools
             pkgs.libjxl
             pkgs.libllvm
@@ -72,9 +53,13 @@
           mkShell {
             buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            CLANG = "${pkgs.libclang.out}";
+            DEP_JXL_LIB = "${pkgs.libjxl.out}";
+
             packages = [
               # Generic DevTools
               # clang-tools must be first before clang
+              pkgs.pkg-config
               pkgs.clang-tools
               pkgs.libjxl
               pkgs.libllvm
