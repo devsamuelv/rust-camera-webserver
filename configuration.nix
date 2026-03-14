e67c706d908c3e1c1888de7508625e4d6ace717d
devsamuelv <realalloutmusic@gmail.com>
2026-03-14 03:42:51 -0400

    Fixed configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,11 @@
 {lib, self, pkgs, ...}: 
 let
-  publickey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2e00/VCmGOXv8NDH+hjuhJLjPu6KSVzwZOv8QwF8Yn samuel@TheSpaceStation";
+  publickey = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2e00/VCmGOXv8NDH+hjuhJLjPu6KSVzwZOv8QwF8Yn samuel@TheSpaceStation"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJm0xjPOnvKlT9gnNlW3L9yuEoISEBLH9Ou3cn4wPBBs"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCkeQ0MSty9182D2XX5mqaN/NF9ijHh8G04BxOkEKM9ABLf0ytQBFDSjHONoYemQgaLRw+dMWW9tNuHC7/6I4PlEOSzJHBlWCjVDwC/D7koT4MhSJAKCFYVCOK4hvf6gf+MkZKaJcbTlNpDHje3WB//emoMvMfht1Eazl6nIUQSeI8GVSOQb8eWVDFHcQSzDAPZejce3McWs9Dl8ILFN123TDXq5n0qzukq0yi5O3U+DzUIXMG7A6V0vuZ1Juks4Jg7J+LBx2X3cLpJ5/s4Gm66DDPGoDmnQHXUFfVqwDq11LeNjl4lNxQxvnq4aamVItWy+pd52FsC/pWswx5M2PO/5hADUuO0RUpQzkvCg3kXGklZ8BdC5vP5j4y5TvT8LrIhEl8O+kqBcWTFonvyoSK8s7xyOZLOk5nzVep/qdnhOsyOMb8hM/xUhyPER4zxHZF9yAjBdyd2OYxqUh09x2zE/jhqYdplEv4Zw2Kt9bSzI1+2VR0lKzBRVW1D4yV96UW7xFKpFi5/1krZm23xG4lKZ7o6Tbv/4+3c6njljv3WiQuNBxligbU6qBXxgIqomDw0ZrZ18DXcXQ8oIuf0FrLerBQdk0H7BcQWcL+6DJOBml/yGi9U4jmw91fdSf1ZVIWx/f3H4yuXGD6EACJBMw5Sdqlzq1q6pMzQ0E0f/6r3rQ=="
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpv4hyo6JLrJQ80rAIb0tQfNro323iyY4YwV/Dpvt88JlnzUGvX160NqTbKZnGkdSrJqtOvGya2aGhdnj/5Wi8PnFoV+E1kWSoyW0dVMEukWrR6pIEzoxEApQ12ROFqsCK9N6IWVB6JHlNH0sqglyQL025WE89OQZEJy2E4w2kAmuRZfYxhMWJkp7cFn7NefMh6YG1J6ZZ75lKcQN65wiJ2sH/d6ujFVS6cX98kU2qkibJ8BuyD83RYBl8U0SoiD1MLd7LqDYHkPEm68mJT/ewP7WgdPPoKuj2+YAKr8Z6DTtHzDW9oehIwxfNssum/ZuBqBKHcfCUPfz1TC8habuxIo5vhgcB/9o/424DPQDzWcdURbBrEw4Tx7Y3ykiyMJ4Z5QAL9RDac+mju7RvIyXt51kXChBSenEMhu7hzFcUIGaXEBnRlu8eaH2da6eebDWc2nEDhG4TU3sb4YA/P9QFdW+JCyS9t7D90UgjhIan3u0kNzzbsieiMc9DETy8sak="
+  ];
 in
 {
   nix.settings = {
@@ -46,17 +51,14 @@ in
     isNormalUser = true;
     home = "/home/rk";
     extraGroups = ["users" "wheel"];
-    openssh.authorizedKeys.keys = [
-      publickey
-    ];
+    openssh.authorizedKeys.keys = publickey;
   };
 
   # enable closed source packages
   nixpkgs.config.allowUnfree = true;
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    publickey
-  ];
+  # Root should be locked down (no network credentials)
+  users.users.root.openssh.authorizedKeys.keys = [];
 
   system.stateVersion = "26.05";
 }
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,116 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Fragment, useState } from 'react';
+import './App.css';
+import { Button, IconButton } from '@mui/material';
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
 
 function App() {
   const [count, setCount] = useState(0)
+    const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: any,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </Fragment>
+  );
 
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <Button variant='contained'>Test</Button>
       </section>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <div>
+        <Button onClick={handleClick}>Open Snackbar</Button>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message="Note archived"
+          action={action}
+        />
+      </div>
 
       <div className="ticks"></div>
       <section id="spacer"></section>
